a0537d5aacdc523bd1ebfd4355c513c1e2eb6db1
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-06-25 14:54:36 +0600

    Create bfs of a graph.cpp

---
+++ bfs of a graph.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        int visited[V] = {0};
+        visited[0] = 1;
+        vector<int> bfs;
+        queue<int> q;
+        q.push(0);
+        while (!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+          // Iterate through all the adjacent nodes of the current node.
+            for (auto it: adj[node]){
+              // If an adjacent node is not visited yet, mark it as visited and push it into the queue.
+                if (!visited[it]){
+                    visited[it] = true;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+}; 
