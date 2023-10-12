d333e1c508e94430664b75f5781eab72b4012160
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-10-12 18:35:50 +0600

    Create word-ladder-1.cpp

---
+++ word-ladder-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+	int wordLadderLength(string startWord, string targetWord, vector<string>& wordList) {
+		// first: string,second: steps
+		queue<pair<string, int>> q;
+		unordered_set<string> st(wordList.begin(), wordList.end());
+		q.push({startWord, 1});
+		st.erase(startWord);
+		while (!q.empty()) {
+			pair<string, int> Pair = q.front();
+			q.pop();
+			string word = Pair.first;
+			int steps = Pair.second;
+			if (word == targetWord) {
+				return steps;
+			}
+			for (int ind = 0; ind < word.size(); ind++) {
+				char original = word[ind];
+				for (char ch = 'a'; ch <= 'z'; ch++) {
+					word[ind] = ch;
+					if (st.find(word) != st.end()) {
+						st.erase(word);
+						q.push({ word, steps + 1 });
+					}
+				}
+				word[ind] = original;
+			}
+
+		}
+		return 0;
+	}
+};
