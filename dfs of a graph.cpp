4f7bd730b0c9dd8c55002de71bd89a22390c92e7
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2024-01-10 16:59:44 +0600

    Update dfs of a graph.cpp
    add comments

--- dfs of a graph.cpp
+++ dfs of a graph.cpp
@@ -1,8 +1,11 @@
 class Solution {
     void dfs(int node, vector<int> adj[], int visited[], vector<int> &ls){
+        // visit the current node
         visited[node] = 1;
         ls.push_back(node);
+        // explore all adjacent nodes of current node
         for (auto it: adj[node]){
+            // only visit if the node is not visited
             if (!visited[it]){
                 dfs(it, adj, visited, ls);
             }
@@ -11,7 +14,6 @@ class Solution {
   public:
     // Function to return a list containing the DFS traversal of the graph.
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
-        // Code here
         int visited[V] = {0};
         vector<int> ls;
         int start = 0;
