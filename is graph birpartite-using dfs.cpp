0f212fae842cd8bc642bd3044e60676579002438
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2024-01-27 16:38:08 +0600

    Update is graph birpartite-using dfs.cpp

--- is graph birpartite-using dfs.cpp
+++ is graph birpartite-using dfs.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    bool check(vector<vector<int>>& graph, vector<int>& colored, int current, bool color) {
-        colored[current] = color;
-        // explore all the adjacent nodes
-        for (auto &adjacentNode: graph[current]){
-            // if adjacent node is not colored, color it 
-            if (colored[adjacentNode] == -1){
-                if (check(graph, colored, adjacentNode, !color) == false){
-                    return false;
-                }
-            }
-            else {
-                if (colored[adjacentNode] == colored[current]){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> colored(n, -1);
-        bool ans = true;
-        for (int i = 0; i < n; i++) {
-            if (colored[i] == -1) {
-                ans = check(graph, colored, i, true);
-            }
-            if (ans == false) {
-                return false;
-            }
-        }
-        return true;
+	bool dfs(int current, vector<int> adj[], vector<int>& colored, int color) {
+		colored[current] = color;
+		for (auto& adjacentNode : adj[current]) {
+			if (colored[adjacentNode] == -1) {
+				if (!dfs(adjacentNode, adj, colored, !color)) {
+					return false;
+				}
+			}
+			else {
+				if (colored[adjacentNode] == colored[current]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	bool isBipartite(int V, vector<int>adj[]) {
+		vector<int> colored(V, -1);
+		for (int i = 0; i < V; i++) {
+			if (colored[i] == -1) {
+				if (!dfs(i, adj, colored, 1)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
-    }
 };
