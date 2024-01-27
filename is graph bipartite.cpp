a353597faf44125928d08cae8ba6e87c0fad6105
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2024-01-27 16:25:33 +0600

    Update is graph bipartite.cpp

--- is graph bipartite.cpp
+++ is graph bipartite.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
-    bool check(vector<vector<int>>& graph, vector<int>& colored, int start) {
-        queue<int> q;
-        // push the first element in the queue
-        q.push(start);
-        // color first cell
-        colored[start] = 0;
+	bool bfs(int current, vector<int> adj[], vector<int>& colored) {
+		queue<int> q;
+		q.push(current);
+		colored[current] = 0;
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			for (auto& adjacentNode : adj[node]) {
+				// check if the adjacent node is already colored
+				if (colored[adjacentNode] != -1) {
+					// check if the adjacent node is already colored with same color as parent
+					if (colored[adjacentNode] == colored[node]) {
+						return false;
+					}
+				}
+				else {
+					// mark with opposite color of parent's node
+					colored[adjacentNode] = !colored[node];
+					q.push(adjacentNode);
+				}
+			}
+		}
+		return true;
+	}
+	bool isBipartite(int V, vector<int>adj[]) {
+		vector<int> colored(V, -1);
+		for (int i = 0; i < V; i++) {
+			if (colored[i] == -1) {
+				if (!bfs(i, adj, colored)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
-        while (!q.empty()) {
-            int current = q.front();
-            q.pop();
-            // explore all neigbour nodes of current node
-            for (auto& adjacentNode : graph[current]) {
-                // if adjacent node is already colored, check if that color is same as curent node color
-                if (colored[adjacentNode] == -1) {
-                    colored[adjacentNode] = !colored[current];
-                    q.push(adjacentNode);
-                }
-                else if (colored[adjacentNode] == colored[current]) {
-                    // if not colored, color the adjancent node with opposite color of current color
-                    return false;
-                    
-                }
-            }
-
-        }
-        return true;
-    }
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> colored(n, -1);
-        bool ans = true;
-        for (int i = 0; i < n; i++) {
-            if (colored[i] == -1) {
-                ans = check(graph, colored, i);
-            }
-            if (ans == false) {
-                return false;
-            }
-        }
-        return true;
-
-    }
 };
 //tc: O(n) + O(2E) where n is for outer loop and 2E is for total degrees.
