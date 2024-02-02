547e3fd4bd34605c99cfe1c09c2fc6fafd04981d
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2024-02-02 18:08:26 +0600

    Update shortest path in undirected graph.cpp

--- shortest path in undirected graph.cpp
+++ shortest path in undirected graph.cpp
@@ -1,49 +1,35 @@
 class Solution {
-public:
-    vector<int> shortestPath(vector<vector<int>>& edges, int N, int M, int src) {
-        vector<int> adjList[N];
-        for (int ind = 0; ind < M; ind++) {
-            int u = edges[ind][0];
-            int v = edges[ind][1];
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
+  public:
+    vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
+        // code here
+        vector<vector<int>> adjList(N);
+        for (auto &it: edges){
+            adjList[it[0]].push_back(it[1]);
+            adjList[it[1]].push_back(it[0]);
         }
-        vector<int> visited(N, 0);
-        // first: node, second: distance
-        queue<pair<int, int>> q;
-      // distance[i] indicates the distance from source
+        // every index indicates distance from source node
         vector<int> distance(N, 1e9);
-        // src node distance is always 0
-        q.push({ src, 0 });
-        visited[src] = 1;
+        queue<int> q;
         distance[src] = 0;
-      
-        while (!q.empty()) {
-            pair<int, int> Pair = q.front();
-            int currentNode = Pair.first;
-            int currentNodeDis = Pair.second;
-
+        q.push(src);
+        while (!q.empty()){
+            int current = q.front();
             q.pop();
-            // explore all neigbour nodes 
-            for (auto & adjacentNode : adjList[currentNode]) {
-                if (!visited[adjacentNode]) {
-                    // if the distance from u to v is less than precalculated distance of
-                    // v, then update the distance
-                    int distanceUToV = currentNodeDis + 1;
-                    if (distanceUToV < distance[adjacentNode]) {
-                        distance[adjacentNode] = distanceUToV;
-                        q.push({ adjacentNode, distanceUToV });
-                        visited[adjacentNode] = 1;
-                    }
+            for (auto &adjacentNode: adjList[current]){
+                // distance from current node to adjacentNode
+                int currentDistance = distance[current] + 1;
+                if (currentDistance < distance[adjacentNode]){
+                    distance[adjacentNode]= currentDistance;
+                    q.push(adjacentNode);
                 }
             }
         }
-        // distance[ind] = INT_MAX means it is unreachable
-        for (int ind = 0; ind < N; ind++) {
-            if (distance[ind] == 1e9) {
-                distance[ind] = -1;
+        for (int i =0; i < N; i++){
+            if (distance[i] == 1e9){
+                distance[i] = -1;
             }
         }
         return distance;
+        
     }
 };
